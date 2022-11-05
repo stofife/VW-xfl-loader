d396033a2c5865c1ed7d803e3b2d1454c6f615f2
stofife <stefandubnicka315@gmail.com>
2022-11-06 00:55:18 +0100

    finished

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os, time
-
 preklad = open("f200_20003_eubergesetzt_de_sk - 7618.txt", "r", encoding="utf-8")
 original = open("f200_20003_de_sk - kópia.xlf", "r", encoding="utf-8")
 loaded = open("f200_20003_de_sk - loaded.xlf", "w", encoding="utf-8")
@@ -13,24 +11,18 @@ for i in range(len(lines)):
     line = lines[i].strip()
     if line != "":
         vals.append(line)
-            
-#print(len(vals))
 
 for i in range(0, len(vals) - 1, 2):
     if not vals[i] in dict.keys():
         dict[vals[i]] = vals[i+1]
     else:
         print(f"String {vals[i]} is already present in dict.keys()!")
-#    print(f"{i}: {vals[i]}:   {vals[i+1]}")
-   
-#print(dict)
 
 lines = original.readlines()
 
 for i in range(1, len(lines)):
     if lines[i].startswith(('<target>', "</target>")):
         l = lines[i].strip()[8:-9]
-        #print(l)
         if l in dict.keys():
             print(" " * len("Writing translation: ") + f" [{l}]  -->  [{dict[l]}]", end="")
             print("\r", end="")
